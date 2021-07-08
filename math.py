ef75517279001c778747826d031f27b099fdf53e
Hritik-PSL <hritik_zurange@persistent.com>
2021-07-08 14:41:53 +0530

    added fn headers to all fns

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+# add fn
 def add(x,y):
     pass
-    
+
+# sub fn    
 def subtract(x,y):
     pass
-    
+
+# mul fn    
 def multiply(x,y):
     pass
-    
+
+# div fn    
 def divide(x,y):
     pass
